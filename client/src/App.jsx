7c6901f2c15ef76da6de09217a467176f7d36364
YaduNR <krishnanyadu77@gmail.com>
2024-01-29 13:45:48 +0530

    create sign up page ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import {BrowserRouter,Routes,Route} from "react-router-dom"
-import SignUp from './pages/Signup'
+import SignUp from "./pages/SignUp"
 import Dashboard from './pages/Dashboard'
 import Projects from './pages/Projects'
 import Home from "./pages/Home"
